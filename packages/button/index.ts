62c020105d9295f43c65a470fdcffae7a981d1d5
yehan <1513849675@qq.com>
2024-08-10 22:22:59 +0800

    feat: 注册button组件

--- packages/button/index.ts
+++ packages/button/index.ts
@@ -1,1 +1,8 @@
 // 入口文件
+
+import Button from './src/button.vue'
+import {App}from 'vue'
+
+Button.install = (app: App) => {
+    app.component(Button.name, Button)
+}
--- packages/button/src/button.vue
+++ packages/button/src/button.vue
@@ -1,10 +1,17 @@
 <template>
-    <button></button>
-    <button></button>
+    <button>按钮</button>
 </template>
 
 <script setup lang="ts">
+import { defineComponent } from 'vue'
 
+defineComponent({
+    name: 'YButton'
+})
+
+defineExpose({
+    name: 'YButton'
+})
 </script>
 
 <style scoped>
